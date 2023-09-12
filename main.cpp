94277fe47a542a95405f613daa4645f2d329181f
Moca <36897041+MocaCDev@users.noreply.github.com>
2023-09-12 18:56:01 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <U8g2lib.h>
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);    //
+
+int lineht;
+
+void setup()
+{
+    u8g2.begin();
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+    u8g2.setFontPosTop();
+    u8g2.clear();
+    u8g2.setCursor(0, 0);
+    u8g2.print("Aidan");
+    u8g2.updateDisplay();
+}
+
+void loop()
+{
+    delay(2000);
+}
